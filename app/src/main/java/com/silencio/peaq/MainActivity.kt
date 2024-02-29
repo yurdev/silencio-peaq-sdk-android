2a32c9c98ba5950a54c92c09e938ee08bcd54c9d
Harsh Mistry <harshm@zignuts.com-at-075929790289>
2024-02-29 11:56:47 +0530

    Initial Commit

---
+++ app/src/main/java/com/silencio/peaq/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.silencio.peaq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
